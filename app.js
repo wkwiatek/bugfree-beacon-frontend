23fb2adffcd6b051b8094e2a349ea44f929bb2f1
Norbert <khozzy@gmail.com>
2015-08-13 22:08:51 +0200

    Put images and JS into dist/ directory

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var app = express();
 app.use(express.static(__dirname + '/dist'));
 
 app.get('*', function(req, res) {
-  res.sendfile('./dist/index.html');
+  res.sendFile('./dist/index.html');
 });
 
 app.listen(process.env.PORT || 3000);
