474de468a268ad31c35b4165b08986af00d3ae90
shark <szczerbicki.pawel@gmail.com>
2015-09-21 22:02:36 +0200

    invoice view

--- src/app/admin/settings/_controllers/settings.controller.js
+++ src/app/admin/settings/_controllers/settings.controller.js
@@ -1,11 +1,22 @@
 'use strict';
 
-function SettingsController() {
+function SettingsController($timeout, invoice) {
 
     var vm = this;
+    vm.invoice = invoice;
 
-    vm.content = 'Settings (from controller)';
-
+    vm.update = function () {
+        vm.invoice.put().then(function () {
+            vm.updatedSuccessfully = true;
+            $timeout(function () {
+                vm.updatedSuccessfully = false;
+            }, 5000);
+        });
+    }
 }
-
+SettingsController.resolve = {
+    invoice: function (Restangular) {
+        return Restangular.one('/customer/invoice-data').get();
+    }
+};
 module.exports = SettingsController;
