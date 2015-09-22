a92fb2d7198e3a20e9df3a9438021376bd2122f4
shark <szczerbicki.pawel@gmail.com>
2015-09-22 23:15:05 +0200

    service battery state

--- src/app/admin/beacons/descriptions/_controllers/beacons-descriptions.controller.js
+++ src/app/admin/beacons/descriptions/_controllers/beacons-descriptions.controller.js
@@ -2,9 +2,19 @@
 
 function BeaconsDescriptionsController(beacons) {
 
-  var vm = this;
-  vm.beacons = beacons;
+    var vm = this;
 
+    vm.determineClass = function (power) {
+        if (power < 15) return 'label-danger';
+        if (power >= 15 && power < 50) return 'label-warning';
+        return 'label-success';
+    };
+
+    angular.forEach(beacons, function (value) {
+        value.class = vm.determineClass(value.state!=null ? value.state.batteryLevel : 0);
+    });
+
+    vm.beacons = beacons;
 }
 
 module.exports = BeaconsDescriptionsController;
