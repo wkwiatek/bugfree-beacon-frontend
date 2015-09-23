396ed86ab48ca9705b9bb8fac2c813fa6b898c22
shark <szczerbicki.pawel@gmail.com>
2015-09-23 22:35:46 +0200

    move class logic to view

--- src/app/admin/beacons/descriptions/_controllers/beacons-descriptions.controller.js
+++ src/app/admin/beacons/descriptions/_controllers/beacons-descriptions.controller.js
@@ -3,17 +3,6 @@
 function BeaconsDescriptionsController(beacons) {
 
     var vm = this;
-
-    vm.determineClass = function (power) {
-        if (power < 15) return 'label-danger';
-        if (power >= 15 && power < 50) return 'label-warning';
-        return 'label-success';
-    };
-
-    angular.forEach(beacons, function (value) {
-        value.class = vm.determineClass(value.state !== null ? value.state.batteryLevel : 0);
-    });
-
     vm.beacons = beacons;
 }
 
