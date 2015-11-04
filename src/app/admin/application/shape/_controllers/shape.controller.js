c427ed9568bfd2e5d0b4ecca157432b89784d748
Norbert <khozzy@gmail.com>
2015-11-04 18:20:43 +0100

    Shape view mockup

---
+++ src/app/admin/application/shape/_controllers/shape.controller.js
@@ -0,0 +1,12 @@
+'use strict';
+
+function ShapeController() {
+  var vm = this;
+
+  vm.shapeNextBtn = function() {
+    console.log('Clicked');
+  };
+
+}
+
+module.exports = ShapeController;
---
+++ src/app/admin/application/shape/index.js
@@ -0,0 +1,4 @@
+'use strict';
+
+module.exports = angular.module('app.application.shape', [])
+  .controller('ShapeController', require('./_controllers/shape.controller.js'));
